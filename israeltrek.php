3f2377e3dfec8b07f205b52d978d665e8aeaadbb
Joel G. Kinney <joelgkinney@fortpoint.me>
2014-11-04 15:07:52 +0000

    Added isread trek

---
+++ israeltrek.php
@@ -0,0 +1,21 @@
+<?php require_once('_header.php'); ?>
+
+<div class="page-header">
+  <h1>WEWORK LABS BOSTON
+ ISRAEL TREK 2015</h1>
+  <aside>
+    <p>Available to Labs Members only, an intimate, curated, guided trek to the Startup Nation</p>
+  </aside>
+</div>
+
+<section>
+<h2>A UNIQUE OPPORTUNITY EXCLUSIVELY FOR LABS MEMBERS</h2>
+<p>Labs members are invited to join Fort Point Legal PC and eTR Express Tours on a trek to the "Startup Nation" 
+to enjoy the same intimate, curated and guided trip in Israel as is planned for MBA students from the
+ world's most prestigious schools. You will meet with innovation leaders, explore cultural treasures
+ and enjoy the wild pleasures only available in this special country. This is an all inclusive trip organized for labs members.
+ Check WeWork listings for an information session soon or check out the Facebook event page
+ <a style="color: black;" href="https://www.facebook.com/events/1500769020185359/" target=" here">here</a>.</p>
+</section>
+
+<?php require_once('_footer.php'); ?>
