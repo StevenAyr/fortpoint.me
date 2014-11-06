1be0759a7d7e0638a2deba08b9dabd4abc567157
Joel G. Kinney <joelgkinney@fortpoint.me>
2014-11-06 17:46:40 +0000

    -m"added new file"

--- israeltrek.php
+++ israeltrek.php
@@ -1,21 +1,42 @@
 <?php require_once('_header.php'); ?>
 
 <div class="page-header">
-  <h1>WEWORK LABS BOSTON
+  <h1>WEWORK BOSTON<br>
  ISRAEL TREK 2015</h1>
   <aside>
-    <p>Available to Labs Members only, an intimate, curated, guided trek to the Startup Nation</p>
+    <p>Available to WeWork Boston Members only, an intimate, curated, guided trek to the Startup Nation</p>
   </aside>
 </div>
 
 <section>
-<h2>A UNIQUE OPPORTUNITY EXCLUSIVELY FOR LABS MEMBERS</h2>
-<p>Labs members are invited to join Fort Point Legal PC and eTR Express Tours on a trek to the "Startup Nation" 
+<h2>Feb. 28 - Mar. 7, 2015</h2>
+<p>all inclusive trip<br> airfare + hotel + guided tours + special events</p>
+</section>
+
+<section>
+<h2>A UNIQUE OPPORTUNITY EXCLUSIVELY FOR BOSTON MEMBERS</h2>
+<p>WeWork Boston members are invited to join Fort Point Legal PC and eTR Express Tours on a trek to the "Startup Nation"
 to enjoy the same intimate, curated and guided trip in Israel as is planned for MBA students from the
  world's most prestigious schools. You will meet with innovation leaders, explore cultural treasures
- and enjoy the wild pleasures only available in this special country. This is an all inclusive trip organized for labs members.
+ and enjoy the exciting nightlife only available in this special country.
  Check WeWork listings for an information session soon or check out the Facebook event page
  <a style="color: black;" href="https://www.facebook.com/events/1500769020185359/" target=" here">here</a>.</p>
 </section>
+
+<section>
+<h2>ISRAEL FROM TOP TO BOTTOM</h2>
+<p>Our guides will take us from Jerusalem to the Dead Sea, from Bedouin camps in the South to vineyards in the North.
+We will visit an air force base, eat with the Druz and relax on the beaches of Tel Aviv. Along the way there will be time to 
+make new friends among WeWork members, meet with Israeli startups and even catch up on your project with a day at WeWork Tel Aviv. </p>
+</section>
+
+<section>
+<h2>AN INSIDE PERSPECTIVE FROM STARTUP NATION</h2>
+<p>At each stop along the way we will have the opportunity to meet and hear from public and private  leaders of Israel's innovation economy.
+ Guest speakers from top startups will give an insider's view of an entrepreneur's life in Israel.
+ We will also have the opportunity to visit and learn from Israeli startups in various industries, making this trip a unique opportunity to
+ explore the country from within.</p></section>
+
+
 
 <?php require_once('_footer.php'); ?>
