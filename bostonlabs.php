6791a813719200a4428e3f52cf91f85f9f94f015
Joel G. Kinney <joelgkinney@fortpoint.me>
2014-11-10 02:18:10 +0000

    added new file

---
+++ bostonlabs.php
@@ -0,0 +1,36 @@
+<?php require_once('_header.php'); ?>
+
+<div class="page-header">
+  <h1>WEWORK BOSTON LABS</h1>
+  <aside>
+    <p>Available to WeWork Members only, an intimate, curated, accelerator style program</p>
+  </aside>
+</div>
+
+<section>
+<h2>WEWORK LABS</h2>
+<p>starting up Boston start ups</p>
+</section>
+
+<section>
+<h2>A UNIQUE OPPORTUNITY EXCLUSIVELY FOR BOSTON MEMBERS</h2>
+<p>WeWork Boston members are invited to join Fort Point Legal PC and eTR Express Tours on a trek to the "Startup Nation"
+to enjoy the same intimate, curated and guided trip in Israel as is planned for MBA students from the
+ world's most prestigious schools. You will meet with innovation leaders, explore cultural treasures
+ and enjoy the exciting nightlife only available in this special country.
+ Check WeWork listings for more information
+ <a style="color: black;" href="https://www.wework.com/locations/unaffiliated/wework-labs/" target=" here">here</a>.</p>
+</section>
+
+<section>
+<h2>ACCELERATE YOUR STARTUP</h2>
+<p>Special events and mentoring for you </p>
+</section>
+
+<section>
+<h2>WORK WITH OTHER MEMEBERS</h2>
+<p>Other great members from whom you can learn</p></section>
+
+
+
+<?php require_once('_footer.php'); ?>
