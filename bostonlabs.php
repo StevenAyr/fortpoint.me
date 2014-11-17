7ceacf46d48387d3c7084bd63466b8aee2ce65cb
Joel G. Kinney <joelgkinney@fortpoint.me>
2014-11-17 00:00:36 +0000

    updated file

--- bostonlabs.php
+++ bostonlabs.php
@@ -3,33 +3,28 @@
 <div class="page-header">
   <h1>WEWORK BOSTON LABS</h1>
   <aside>
-    <p>Available to WeWork Members only, an intimate, curated, accelerator style program</p>
+    <p>The benefits of WeWork membership with the advantages of a mentor-driven, accelerator style program for seed stage companies.</p>
   </aside>
 </div>
 
 <section>
-<h2>WEWORK LABS</h2>
-<p>starting up Boston start ups</p>
+<h2>COMMUNITY</h2>
+<p>WeWork Labs is a community within our community–an open floor concept designed to provide additional support for members who seek it. While some of our locations have a strong concentration in a particular field like technology, design or entertainment.</p>
 </section>
 
 <section>
-<h2>A UNIQUE OPPORTUNITY EXCLUSIVELY FOR BOSTON MEMBERS</h2>
-<p>WeWork Boston members are invited to join Fort Point Legal PC and eTR Express Tours on a trek to the "Startup Nation"
-to enjoy the same intimate, curated and guided trip in Israel as is planned for MBA students from the
- world's most prestigious schools. You will meet with innovation leaders, explore cultural treasures
- and enjoy the exciting nightlife only available in this special country.
- Check WeWork listings for more information
- <a style="color: black;" href="https://www.wework.com/locations/unaffiliated/wework-labs/" target=" here">here</a>.</p>
+<h2>MENTORING</h2>
+<p>Labs mentors are entrepreneurs, investors, marketers, finance professionals and others who volunteer their expertise to help entrepreneurs. In addition to the official mentorship pairings, MassChallege facilitates countless other meetings between experts and entrepreneurs. Mentors and speakers host office hours to hold brief meetings with entrepreneurs looking for advice or introductions.</p>
 </section>
 
 <section>
-<h2>ACCELERATE YOUR STARTUP</h2>
-<p>Special events and mentoring for you </p>
+<h2>EVENTS</h2>
+<p>Labs members will have access to exclusive programs created around their needs. Intimate networking events with local investors, talks from industry leaders, demo nights, pitch events, Labs Member parties and even a <a style="color: black;" href="http://fortpoint.me/israeltrek.php" target="guided trek to Israel"> guided trek to Israel</a> just to name a few </p>
 </section>
 
 <section>
-<h2>WORK WITH OTHER MEMEBERS</h2>
-<p>Other great members from whom you can learn</p></section>
+<h2>APPLY TODAY</h2>
+<p>Learn more about labs and how to apply at <a style="color: black;" href="https://www.wework.com/locations/unaffiliated/wework-labs/" target="wework.com/labs">wework.com/labs</a></p></section>
 
 
 
